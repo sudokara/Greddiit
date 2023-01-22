2c0f922bd8f6c08ae82d2e0a55f6b60c5c935f31
Shrikara A <shrikarame@gmail.com>
2023-01-22 19:31:48 +0530

    added comments to hello world server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
+/// Imports
 const express = require('express');
 const dotenv = require('dotenv').config();
+///
+
+// Initialisations
 const app = express();
 
 
+// Homepage
 app.get('/', (req, res) => {
     res.status(200).send("<h1>Hello world</h1>");
 });
 
+
+// Start server on specified port
 app.listen(process.env.BACKEND_PORT, () => {
     console.log("Server started");
 });
