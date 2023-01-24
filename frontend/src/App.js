a13fbc4899f3eb66542e2d66cbfc2dc03b8b8dd9
Shrikara A <shrikarame@gmail.com>
2023-01-24 11:39:51 +0530

    deleted mui frontend and added daisyui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,8 @@
-import "./App.css";
-import {BrowserRouter, Route, Routes } from "react-router-dom";
-
-import NotFound from "./components/NotFound";
-import Login from "./components/Login"
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        {/* <Route path="/me" element={<Profile />}></Route> */}
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <button className="btn btn-primary">DaisyUI Installed</button>  
+    </div>
   );
 }
 
