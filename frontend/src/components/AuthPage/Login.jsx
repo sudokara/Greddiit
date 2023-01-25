fca4878496444cd9b43479fcc0b1ee08b984cd16
Shrikara A <shrikarame@gmail.com>
2023-01-25 09:41:21 +0530

    Wrapped signup and login in form element

--- frontend/src/components/AuthPage/Login.jsx
+++ frontend/src/components/AuthPage/Login.jsx
@@ -44,74 +44,76 @@ const Login = ({ isLoading, setIsLoading }) => {
               required
             />
           </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="notqwerty123"
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={password}
-              disabled={isLoading}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <label
-              htmlFor="forgot-password-modal"
-              className="label cursor-pointer"
-            >
-              Forgot Password?
-            </label>
-            <input
-              type="checkbox"
-              id="forgot-password-modal"
-              className="modal-toggle"
-            />
-            <label
-              htmlFor="forgot-password-modal"
-              className="modal cursor-pointer"
-            >
-              <label className="modal-box relative" htmlFor="">
-                <h3 className="text-lg font-bold">Quite Sad!</h3>
-                <p className="py-4">
-                  You can think harder, because that's the only way you can get
-                  your password back; even I don't know what it is.
-                </p>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
               </label>
-            </label>
-          </div>
-          <div className={`form-control mt-6`}>
-            <button
-              disabled={!email || !password}
-              className={`btn btn-primary ${isLoading ? "loading" : ""}`}
-              onClick={handleLogin}
-            >
-              <span>Login</span>
-            </button>
-            {showError ? (
-              <div className="alert alert-error shadow-lg mt-5">
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="stroke-current flex-shrink-0 h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  <span>Incorrect username or password.</span>
+              <input
+                type="password"
+                placeholder="notqwerty123"
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={password}
+                disabled={isLoading}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <label
+                htmlFor="forgot-password-modal"
+                className="label cursor-pointer"
+              >
+                Forgot Password?
+              </label>
+              <input
+                type="checkbox"
+                id="forgot-password-modal"
+                className="modal-toggle"
+              />
+              <label
+                htmlFor="forgot-password-modal"
+                className="modal cursor-pointer"
+              >
+                <label className="modal-box relative" htmlFor="">
+                  <h3 className="text-lg font-bold">Quite Sad!</h3>
+                  <p className="py-4">
+                    You can think harder, because that's the only way you can
+                    get your password back; even I don't know what it is.
+                  </p>
+                </label>
+              </label>
+            </div>
+            <div className={`form-control mt-6`}>
+              <button
+                disabled={!email || !password}
+                className={`btn btn-primary ${isLoading ? "loading" : ""}`}
+                onClick={handleLogin}
+              >
+                <span>Login</span>
+              </button>
+              {showError ? (
+                <div className="alert alert-error shadow-lg mt-5">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="stroke-current flex-shrink-0 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <span>Incorrect username or password.</span>
+                  </div>
                 </div>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </form>
         </div>
       </div>
     </>
--- frontend/src/components/AuthPage/Signup.jsx
+++ frontend/src/components/AuthPage/Signup.jsx
@@ -13,22 +13,6 @@ const Signup = ({ isLoading, setIsLoading }) => {
 
   const navigate = useNavigate();
 
-  // const disableSignupButton = () => {
-  //   if (
-  //     !email ||
-  //     !password ||
-  //     !firstName ||
-  //     !lastName ||
-  //     !username ||
-  //     !contactNo ||
-  //     isLoading
-  //   ) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // };
-
   const handleSignup = (e) => {
     setIsLoading(true);
     setTimeout(() => {
@@ -41,125 +25,127 @@ const Signup = ({ isLoading, setIsLoading }) => {
     <>
       <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
         <div className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">First Name</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Count"
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={firstName}
-              disabled={isLoading}
-              onChange={(e) => setFirstName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Last Name</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Dracula"
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={lastName}
-              disabled={isLoading}
-              onChange={(e) => setLastName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Username</span>
-            </label>
-            <input
-              type="text"
-              placeholder="dracula7"
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={username}
-              disabled={isLoading}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="example@example.com"
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={email}
-              disabled={isLoading}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Age</span>
-            </label>
-            <input
-              type="number"
-              min={13}
-              max={120}
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={age}
-              disabled={isLoading}
-              onChange={(e) => setAge(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Contact</span>
-            </label>
-            <input
-              type="tel"
-              pattern="[0-9]{10}"
-              placeholder="1234567890"
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={contactNo}
-              disabled={isLoading}
-              onChange={(e) => setContactNo(e.target.value)}
-              required
-            />
-          </div>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">First Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Count"
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={firstName}
+                disabled={isLoading}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Last Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Dracula"
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={lastName}
+                disabled={isLoading}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Username</span>
+              </label>
+              <input
+                type="text"
+                placeholder="dracula7"
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={username}
+                disabled={isLoading}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="example@example.com"
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={email}
+                disabled={isLoading}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Age</span>
+              </label>
+              <input
+                type="number"
+                min={13}
+                max={120}
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={age}
+                disabled={isLoading}
+                onChange={(e) => setAge(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Contact</span>
+              </label>
+              <input
+                type="tel"
+                pattern="[0-9]{10}"
+                placeholder="1234567890"
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={contactNo}
+                disabled={isLoading}
+                onChange={(e) => setContactNo(e.target.value)}
+                required
+              />
+            </div>
 
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="iltransylvania"
-              className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
-              value={password}
-              disabled={isLoading}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="iltransylvania"
+                className="input input-bordered active:border-indigo-500/100 hover:border-indigo-500/100 focus:border-indigo-500/100"
+                value={password}
+                disabled={isLoading}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
 
-          <div className={`form-control mt-6`}>
-            <button
-              disabled={
-                !email ||
-                !password ||
-                !firstName ||
-                !lastName ||
-                !contactNo ||
-                !username ||
-                isLoading
-              }
-              className={`btn btn-primary ${isLoading ? "loading" : ""}`}
-              onClick={handleSignup}
-            >
-              <span>Signup</span>
-            </button>
-          </div>
+            <div className={`form-control mt-6`}>
+              <button
+                disabled={
+                  !email ||
+                  !password ||
+                  !firstName ||
+                  !lastName ||
+                  !contactNo ||
+                  !username ||
+                  isLoading
+                }
+                className={`btn btn-primary ${isLoading ? "loading" : ""}`}
+                onClick={handleSignup}
+              >
+                <span>Signup</span>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
