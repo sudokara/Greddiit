870884d105c100e2779b8815bb01049f7ea719ef
Shrikara A <shrikarame@gmail.com>
2023-02-04 20:28:13 +0530

    updated usermodel

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,16 +1,67 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    contactnum: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    followers: {
+      type: [String],
+      required: false,
+    },
+    following: {
+      type: [String],
+      required: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const userSchema = new Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     }
+// });
+
+// module.exports = mongoose.model('User', userSchema);
