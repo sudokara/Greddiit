bfc7f673eb539a0d4c42cb308db66c2b21dcc24a
Shrikara A <shrikarame@gmail.com>
2023-02-09 18:35:32 +0530

    changed class to className in 404 page

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div class="h-screen w-full flex flex-col justify-center items-center bg-[#282a36]">
-      <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
-      <div class="bg-[#ff79c6] px-2 text-sm rounded rotate-12 absolute">
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-[#282a36]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
+      <div className="bg-[#ff79c6] px-2 text-sm rounded rotate-12 absolute">
         Page Not Found
       </div>
-        <a class="relative inline-block text-sm font-medium text-[#ff79c6] group active:text-orange-500 focus:outline-none focus:ring">
-          <button class="btn btn-primary mt-5">
+        <div className="relative inline-block text-sm font-medium text-[#ff79c6] group active:text-orange-500 focus:outline-none focus:ring">
+          <button className="btn btn-primary mt-5">
             <Link to="/" replace>Home</Link>
           </button>
-        </a>
+        </div>
     </div>
   );
 };
