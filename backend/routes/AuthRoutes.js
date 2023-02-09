d33ea983c2f99f93926ed406cd7c6df2d9ca670f
Shrikara A <shrikarame@gmail.com>
2023-02-09 15:42:41 +0530

    Check auth route added

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -3,16 +3,14 @@ const router = express.Router();
 const AuthController = require("../controllers/AuthController");
 const loginLimiter = require("../middleware/RateLimiter");
 
-router.route("/")
-    .post(loginLimiter, AuthController.login)
+router.route("/").post(loginLimiter, AuthController.login);
 
-router.route("/refresh")
-    .get(AuthController.refresh)
+router.route("/refresh").get(AuthController.refresh);
 
-router.route("/register")
-    .post(loginLimiter, AuthController.register)
+router.route("/check").get(AuthController.checkAuth);
 
-router.route("/logout")
-    .post(AuthController.logout)
+router.route("/register").post(loginLimiter, AuthController.register);
 
-module.exports = router
+router.route("/logout").post(AuthController.logout);
+
+module.exports = router;
